ca57461f77ee3420ce29176c5306a6bd3ed5fbdc
keruzam <mazurek.zg@gmail.com>
2024-01-05 23:46:04 +0100

    project init

---
+++ src/main/java/pl/keruzam/emaillabsmongodb/EmaillabsMongodbApplication.java
@@ -0,0 +1,13 @@
+package pl.keruzam.emaillabsmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaillabsMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmaillabsMongodbApplication.class, args);
+	}
+
+}
